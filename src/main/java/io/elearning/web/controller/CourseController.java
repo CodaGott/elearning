119c10f5feebb99dfa38ea20712f125007c442fb
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-02-17 21:34:26 +0100

    user controller

---
+++ src/main/java/io/elearning/web/controller/CourseController.java
@@ -0,0 +1,7 @@
+package io.elearning.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+}
---
+++ src/main/java/io/elearning/web/controller/UserController.java
@@ -0,0 +1,66 @@
+package io.elearning.web.controller;
+
+import io.elearning.data.dto.UserDto;
+import io.elearning.exceptions.UserException;
+import io.elearning.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("signup")
+    public ResponseEntity<?> createUserAccount(@RequestBody UserDto userDto){
+        try {
+            userService.createUser(userDto);
+            return new ResponseEntity<>("Account created successfully.", HttpStatus.CREATED);
+        }catch (UserException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("admin-signup")
+    public ResponseEntity<?> createAdminAccount(@RequestBody UserDto userDto){
+        try {
+            userService.createAdmin(userDto);
+            return new ResponseEntity<>("Admin Account created successfully", HttpStatus.CREATED);
+        }
+        catch (UserException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @PostMapping("teacher-signup")
+    public ResponseEntity<?> createTeacherAccount(@RequestBody UserDto userDto){
+        try {
+            userService.createTeacher(userDto);
+            return new ResponseEntity<>("Teacher account created", HttpStatus.CREATED);
+        }
+        catch (UserException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("allusers")
+    public ResponseEntity<?> getAllUsers(){
+        userService.getAllUsers();
+        return new ResponseEntity<>("All users", HttpStatus.OK);
+    }
+
+    @GetMapping("user/{userId}")
+    public ResponseEntity<?> getUserById(@PathVariable Long userId){
+        try {
+            userService.getUserById(userId);
+            return new ResponseEntity<>("User with id of "+ userId + "found", HttpStatus.FOUND);
+        }catch (UserException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
